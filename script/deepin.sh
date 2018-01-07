f5b5c447a7605017b2425f2eccf29dc289a42726
ii6uu99 <ii6uu99@163.com>
2018-01-07 21:16:01 +0800

    aa

--- script/deepin.sh
+++ script/deepin.sh
@@ -1,6 +1,7 @@
 #sudo vim /etc/apt/sources.list
 
 sudo tee /etc/apt/sources.list <<-'EOF'
+#中科大源
 deb [by-hash=force] http://mirrors.ustc.edu.cn/deepin unstable main contrib non-free
 #deb-src http://mirrors.ustc.edu.cn/deepin unstable main contrib non-free
 EOF
