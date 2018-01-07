20d5890888c47e6985fcf0f37a09e69d9195814d
ii6uu99 <ii6uu99@163.com>
2018-01-07 20:55:01 +0800

    aa

---
+++ script/deepin.sh
@@ -0,0 +1,17 @@
+sudo tee /etc/apt/sources.list <<-'EOF'
+deb http://mirrors.aliyun.com/ubuntu raring main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu raring-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu raring-updates main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu raring-proposed main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu raring-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu raring main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu raring-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu raring-updates main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu raring-proposed main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu raring-backports main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/deepin raring main universe non-free
+deb-src http://mirrors.aliyun.com/deepin raring main universe non-free
+#deb http://mirrors.aliyun.com/deepin raring-updates main universe non-free
+#deb-src http://mirrors.aliyun.com/deepin raring-updates main universe non-free
+EOF
