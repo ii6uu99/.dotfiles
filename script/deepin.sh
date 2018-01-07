0cf8b68cb2afa894def44dcdb326a5e071fade52
ii6uu99 <ii6uu99@163.com>
2018-01-07 21:13:01 +0800

    aa

--- script/deepin.sh
+++ script/deepin.sh
@@ -1,21 +1,7 @@
 #sudo vim /etc/apt/sources.list
 
 sudo tee /etc/apt/sources.list <<-'EOF'
-deb http://mirrors.aliyun.com/ubuntu raring main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu raring-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu raring-updates main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu raring-proposed main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu raring-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu raring main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu raring-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu raring-updates main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu raring-proposed main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu raring-backports main restricted universe multiverse
-
-deb http://mirrors.aliyun.com/deepin raring main universe non-free
-deb-src http://mirrors.aliyun.com/deepin raring main universe non-free
-#deb http://mirrors.aliyun.com/deepin raring-updates main universe non-free
-#deb-src http://mirrors.aliyun.com/deepin raring-updates main universe non-free
-
+deb [by-hash=force] http://mirrors.ustc.edu.cn/deepin unstable main contrib non-free
+#deb-src http://mirrors.ustc.edu.cn/deepin unstable main contrib non-free
 EOF
 sudo apt-get update
