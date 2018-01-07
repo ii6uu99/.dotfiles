015201229f63e66dc57fadee4900787325b431cc
ii6uu99 <ii6uu99@163.com>
2018-01-07 22:54:01 +0800

    aa

--- script/aliyun.sh
+++ script/aliyun.sh
@@ -9,12 +9,12 @@ rm -rf /var/lib/docker
 
 #脚本来源 https://github.com/rancher/install-docker
 #docker安装包来源
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/rancher/install-docker/master/1.10.3.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/rancher/install-docker/master/1.11.2.sh)"
 
 #curl -L https://raw.githubusercontent.com/rancher/install-docker/master/1.10.3.sh | sh
 
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/rancher/install-docker/master/17.09.0.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/rancher/install-docker/master/17.09.0.sh)"
 
 
 
@@ -36,7 +36,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/rancher/install-docker/mas
 #docker -v 客户端版本要大于1.10.0
 
 #通过修改daemon配置文件/etc/docker/daemon.json来使用加速器：
-sudo rm var/run/docker.sock
+#sudo rm var/run/docker.sock
 
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
