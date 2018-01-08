9cf6c997f53de3aebf1e24593ffd678de1cad581
ii6uu99 <ii6uu99@163.com>
2018-01-08 19:12:01 +0800

    aa

--- script/jiasu.sh
+++ script/jiasu.sh
@@ -4,10 +4,11 @@
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
-  #"registry-mirrors": ["https://r2t47usc.mirror.aliyuncs.com"]
-  "registry-mirrors":["https://docker.mirrors.ustc.edu.cn"]
+  "registry-mirrors": ["https://r2t47usc.mirror.aliyuncs.com"]
 }
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
+
+#"registry-mirrors":["https://docker.mirrors.ustc.edu.cn"]
