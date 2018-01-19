01b2038781f604a1488cd0fa569099159c549394
ii6uu99 <ii6uu99@163.com>
2018-01-19 17:30:02 +0800

    aa

--- script/docker-compose.sh
+++ script/docker-compose.sh
@@ -6,3 +6,14 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-c
 
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+
+#下载目前的最新版：https://github.com/docker/compose/releases/tag/1.14.0-rc2
+
+#docker-compose-Linux-x86_64
+#rename docker-compose-Linux-x86_64 docker-compose
+#cp docker-compose /usr/local/bin/
+#chmod +x /usr/local/bin/docker-compose
+#查看版本
+#docker-compose version
+
