a23e6c3c330aac26acecbe3c1e9c114ef3e903c4
ii6uu99 <ii6uu99@163.com>
2018-01-21 18:10:01 +0800

    aa

--- script/deepin-install-docker.sh
+++ script/deepin-install-docker.sh
@@ -1,3 +1,5 @@
+#deepin官网安装docker
+#https://wiki.deepin.org
 #deepin官网安装docker   https://wiki.deepin.org/index.php?title=Docker
 
 #卸载以前版本
@@ -15,18 +17,27 @@ sudo apt-key fingerprint 0EBFCD88
 #cat /etc/debian_version，再根据版本号对应的代号替换上面命令的wheezy即可
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian wheezy stable"
 
+#更新仓库
+sudo apt-get update
+
+#安装docker-ce
+sudo apt-get install docker-ce
+
+查看安装的版本信息
+docker version
+
+验证docker是否被正确安装并且能够正常使用
+sudo docker run alpine
 
 
-
-
-
-
-
-
-
-
-
-
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://registry.docker-cn.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 
 
@@ -38,5 +49,29 @@ sudo gpasswd -a ${USER} docker
 sudo service docker restart
 #切换当前会话到新 group 或者重启 X 会话
 newgrp - docker
+
+
+
+#禁止开机自启
+#默认情况下Docker是开机自启的，如果我们想禁用开机自启，可以通过安装chkconfig命令来管理Deepin自启项
+
+# 安装chkconfig
+#sudo apt-get install chkconfig
+
+# 移除自启
+#sudo chkconfig --del docker
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
