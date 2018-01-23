db329cb62163eab1dc993e2a0f0e86778563eec2
ii6uu99 <ii6uu99@163.com>
2018-01-23 11:10:01 +0800

    aa

--- docker/面向桌面应用程序的Docker容器/deepin5/deepin6/source.sh
+++
@@ -1,4 +0,0 @@
-tee /etc/apt/sources.list <<-'EOF'
-deb [by-hash=force] http://mirrors.aliyun.com/deepin unstable main contrib non-free 
-deb-src http://mirrors.aliyun.com/deepin unstable main contrib non-free
-EOF
